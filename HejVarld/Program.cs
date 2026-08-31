c6c7288262497bc729af55509b61dc2c25271ade
emilchau07-jpg <emil.chau07@gmail.com>
2026-08-31 11:26:28 +0200

    enkel if/else

--- HejVarld/Program.cs
+++ HejVarld/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Console.WriteLine("Hello, World!");
+
+string name = "Emil";
+int age = 19;
+double height = 1.72;
+bool isDeveloper = false;
+
+Console.WriteLine($"Jag heter {name} och jag är {age} år gammal");
+Console.WriteLine($"Jag är {height} meter lång");
+
+if (isDeveloper)
+{
+    Console.WriteLine("Jag är en utvecklare");
+}
+else
+{
+    Console.WriteLine("Jag är inte en utvecklare än");
+}

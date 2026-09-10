19d69501ec753879fcd88ea6ce45671bef01bf2e
emilchau07-jpg <emil.chau07@gmail.com>
2026-09-10 15:07:32 +0200

    Restart

--- Kunskapskontroll 1/Del-A/Del-A.cs
+++ Kunskapskontroll 1/Del-A/Del-A.cs
@@ -1,5 +1,3 @@
-//Rubrik till inköpslistan
-
 //En lista där användaren ska ange namnet på varje vara
 
 //En lista där användaren ska ange priset för varje vara (i heltal)
@@ -15,42 +13,8 @@ Krav för godkänt:
 
 */
 
-
+//Rubrik och skapade listorna
 Console.WriteLine("Inköpslista");
 List<string> produkt = [];
 List<int> pris = [];
 
-static string Ask(string question)
-{
-    Console.WriteLine(question + "");
-    return Console.ReadLine()!;
-}
-
-while (true)
-{
-    Console.Clear();
-    string[] menu = [
-        "Vad vill du göra?",
-        "1. Lägga till en produkt",
-        "2. Ta bort en produkt",
-        "3 Klar med listan"
-    ];
-    string choice = Ask(string.Join("\n", menu) + "\n");
-    Console.Clear();
-    if (choice == "1")
-    {
-        //Lägger till en vara/produkt
-        produkt.Add(Ask("Vilken vara vill du lägga till: "));
-        pris.Add(int.Parse(Ask("Vad är priset på varan (heltal): ")));
-    }
-    else if (choice == "2")
-    {
-        //Tar bort en produkt
-        int index = int.Parse(Ask("Vilken produkt vill du ta bort (agne nummer)? ")) - 1;
-    }
-    else if (choice == "3")
-    {
-        //Ska visa alla produkter och totalsumman, avslutar programmet med 1
-
-    }
-}

bc362dac5606fd79bd734295ba3cee7ec80bc981
emilchau07-jpg <emil.chau07@gmail.com>
2026-09-14 14:56:10 +0200

    En bra början

--- Lektioner/Program.cs
+++ Lektioner/Program.cs
@@ -1,1 +1,10 @@
 ﻿Console.WriteLine("Välkommen till banken!");
+
+User user = new("Emil", "Chau");
+
+Console.WriteLine("\n===Kontoinformation===");
+Console.WriteLine($"{user.firstname} {user.lastname}");
+Console.WriteLine(user.kontonummer);
+Console.WriteLine("======================");
+
+Console.WriteLine($"Saldo: {user.saldo}");
--- Lektioner/UpgBankkonto.cs
+++ Lektioner/UpgBankkonto.cs
@@ -1,9 +1,9 @@
-class bankKonto()
+public class User(string firstname, string lastname)
 {
-    int kontonummer = 123456789;
-    string firstname = "Emil";
-    string lastname = "Chau";
-    double saldo = 0;
+    public int kontonummer { get; } = 123456789;
+    public string firstname { get; } = firstname;
+    public string lastname { get; } = lastname;
+    public double saldo = 25;
     double ränta = 0;
     double kreditgräns = 50_000;
     bool kontostatus = true;

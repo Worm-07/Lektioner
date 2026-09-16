48ecb585b8fad217563716c10957f93b8629da8a
emilchau07-jpg <emil.chau07@gmail.com>
2026-09-16 13:46:57 +0200

    Translating dictionary

--- Lektioner/Dictonary/Dictonary_intro.cs
+++ Lektioner/Dictonary/Dictonary_intro.cs
@@ -6,3 +6,25 @@ Dictionary<string, int> ages = new()
 };
 
 Console.WriteLine($"Emil is {ages["Emil"]} years old");
+
+
+
+Dictionary<string, string> svToEn = new()
+{
+    ["hund"] = "dog",
+    ["katt"] = "cat",
+    ["fågel"] = "bird",
+    ["råtta"] = "rat",
+    ["Patrik"] = "Patrick(med ck)"
+};
+Console.WriteLine("Vilket ord vill du översätta till engelska? ");
+string? toTranslate = Console.ReadLine();
+
+if (svToEn.ContainsKey(toTranslate!))
+{
+    Console.WriteLine($"{toTranslate} är {svToEn[toTranslate]} på engelska");
+}
+else
+{
+    Console.WriteLine($"{toTranslate} finns inte i vår engelska ordlista");
+}
